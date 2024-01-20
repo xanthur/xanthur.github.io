3ef24c76986e521f867c77b93d0e3e9a035b9e4c
juxive <76982348+juxive@users.noreply.github.com>
2024-01-19 22:53:20 -0400

    satellite overhaul
    new satellite model, aligned page thumbnails, and improved browser compatibility

--- js/app.js
+++ js/app.js
@@ -1,14 +1,15 @@
 var open = 0;
+
 function moveSatellite(){
     var satellite = document.getElementById("satellite")
     
 
     if (open == 0){
-    satellite.style.transform = "translate(-15%, 0%)";
+    satellite.style.transform = "translate(-30%, -40%)";
     open = 1;
     }
     else{
-        satellite.style.transform = "translate(-60%, 0%)";
+        satellite.style.transform = "translate(-55%, -40%)";
     open = 0;
     }
     
